48705e9f1150e17c86cc52aa200ff7b4dbc05e3f
unknown <abdelhedi.hlel@oyez.fr>
2019-01-10 09:31:22 +0100

    file organisation

---
+++ src/components/Clients.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+const Container = () => (
+    <section id="clients" className="section one-page-section second-bg">
+        <div className="container">
+            <div className="section-title">
+                <h2>Testimonials</h2>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                    <div className="owl-carousel testimonial-slider">
+
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-5.jpg" alt="tes-img-5" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>Amber Cottle</h4>
+                                    <p>Web Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-3.jpg" alt="tes-img-3" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>John Doe</h4>
+                                    <p>Ceo Founder</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-4.jpg" alt="tes-img-4" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>Alvin Jewel</h4>
+                                    <p>Creative Head</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-2.jpg" alt="tes-img-2" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>Rebecca Johansson</h4>
+                                    <p>Project Manager</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-1.jpg" alt="tes-img-1" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>Mark Smith</h4>
+                                    <p>Creative Head</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="one-testimonial">
+                            <div className="testimonial-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh facilisis bibendum dolor feugiat.</p>
+                            </div>
+                            <div className="testimonial-person">
+                                <figure>
+                                    <img src="http://phydev.web44.net/erizo/img/t-6.jpg" alt="tes-img-6" />
+                                </figure>
+                                <div className="testimonial-inf">
+                                    <h4>Audley Carson</h4>
+                                    <p>Front End Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+)
+
+export default Container;
+
---
+++ src/components/Service.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const Service = () => (
+    <section id="services" className="section one-page-section second-bg">
+        <div className="container">
+            <div className="section-title">
+                <h2>My services</h2>
+            </div>
+
+            <div className="row">
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service">
+                        <div className="service-icon">
+                            <i className="fa fa-html5 effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>WEB DEVELOPMENT</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service bordered">
+                        <div className="service-icon">
+                            <i className="fa fa-laptop effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>WEBSITE DESIGN</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service">
+                        <div className="service-icon">
+                            <i className="fa fa-mobile effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>APP DEVELOPMENT</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="clear-float"></div>
+                <div className="services-separator"></div>
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service">
+                        <div className="service-icon">
+                            <i className="fa fa-picture-o effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>GRAPHIC DESIGN</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service bordered">
+                        <div className="service-icon">
+                            <i className="fa fa-camera-retro effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>CREATIVE PHOTOGRAPHY</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-xs-12 col-md-4">
+                    <div className="single-service">
+                        <div className="service-icon">
+                            <i className="fa fa-life-ring effect"></i>
+                        </div>
+                        <div className="service-content">
+                            <h3>ONLINE SUPPORT</h3>
+                            <div className="s-line"></div>
+                            <p>Nullam porttitor porta augue vel iaculis. Pellentesque a pretium erat. Maecenas semper laoreet dapibus et quatre malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+)
+
+export default Service;
